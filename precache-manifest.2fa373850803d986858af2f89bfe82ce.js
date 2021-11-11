240c1053ae5cfe8ec10e4a965d9d44ab2eca53d4
Stanley Luong <slluong@uw.edu>
2021-11-11 04:35:33 -0800

    Updates

---
+++ precache-manifest.2fa373850803d986858af2f89bfe82ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e51f4ae04b0b0bceb1245f35cec8d2e",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "065c699c4d8a5d9fbe0a",
+    "url": "/snake/static/js/2.491d0b30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake/static/js/2.491d0b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f11024b8063f719099f",
+    "url": "/snake/static/js/main.47635df3.chunk.js"
+  },
+  {
+    "revision": "757e336a23d9715a1cc8",
+    "url": "/snake/static/js/runtime-main.0646e259.js"
+  }
+]);
